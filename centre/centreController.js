309627dd8bd0bce32e6c42617067617efcaf4526
Mighty Eagle <mightyeaglecorp@gmail.com>
2024-02-11 18:22:08 +0100

    made changes

--- centre/centreController.js
+++ centre/centreController.js
@@ -1,3 +1,10 @@
+import candidateModel from "../downloadAndUpload/candidateModel.js";
+import examScheduleModel from "../downloadAndUpload/examScheduleModel.js";
+import examinationModel from "../downloadAndUpload/examinationModel.js";
+import questionBankCategoryModel from "../downloadAndUpload/questionBankCategoryModel.js";
+import questionBankModel from "../downloadAndUpload/questionBankModel.js";
+import responseModel from "../downloadAndUpload/responseModel.js";
+import subjectModel from "../downloadAndUpload/subjectModel.js";
 import centreModel from "./centreModel.js";
 
 export const saveCentre = async (req, res) => {
@@ -18,4 +25,16 @@ export const loginCentre = async (req, res) => {
   if (!centre) return res.status(401).send("Centre credentials incorrect");
 
   res.send(centre);
+};
+
+export const resetServer = async (req, res) => {
+  await candidateModel.deleteMany();
+  await examinationModel.deleteMany();
+  await responseModel.deleteMany();
+  await examScheduleModel.deleteMany();
+  await questionBankCategoryModel.deleteMany();
+  await questionBankModel.deleteMany();
+  await subjectModel.deleteMany();
+
+  res.send("Server reset");
 };
--- centre/centreRouter.js
+++ centre/centreRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { loginCentre, saveCentre } from "./centreController.js";
+import { loginCentre, resetServer, saveCentre } from "./centreController.js";
 
 const centreRouter = express.Router();
 
@@ -7,6 +7,7 @@ const rootPath = (path) => `/aguila/centre/${path}`;
 
 centreRouter
   .post(rootPath("save"), saveCentre)
-  .post(rootPath("login"), loginCentre);
+  .post(rootPath("login"), loginCentre)
+  .get(rootPath("resetserver"), resetServer);
 
 export default centreRouter;
