d71deace2be03c6c469c6741711ca4764336fbde
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2024-10-15 10:16:25 +0100

    made changes

--- database.js
+++ database.js
@@ -4,7 +4,10 @@ const database = "mongodb://127.0.0.1:27017/STERLING";
 
 export const ConnectDatabase = () => {
   mongoose
-    .connect(database, {})
+    .connect(database, {
+      connectTimeoutMS: 60000,
+      serverSelectionTimeoutMS: 60000,
+    })
     .then(() => console.log("Database connected successfully"))
     .catch((e) => console.log(`Error opening DB ${new Error(e).message}`));
 };
